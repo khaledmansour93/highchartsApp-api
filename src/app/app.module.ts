b8e7a09d65d0c0df66324fc32b9a136ea41983ac
Khaled Mansour <khaledmansour818@gmail.com>
2023-04-17 14:29:43 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ChartModule } from 'angular-highcharts';
+import { HttpClientModule } from '@angular/common/http';  // Module to make HTTP requests like GET & POST, etc. to the back end server
 import { AppComponent } from './app.component';
+import { HighchartsChartComponent } from './highcharts-chart/highcharts-chart.component'; // Module needed to use Highcharts chart
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HighchartsChartComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ChartModule, // add ChartModule to your imports
+    HttpClientModule  // add HttpClientModule to your imports
   ],
   providers: [],
   bootstrap: [AppComponent]
